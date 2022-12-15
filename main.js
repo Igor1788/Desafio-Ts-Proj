ca3c3b1a9435c74107809c3d42949b0257c40b90
Igor <igor1788@gmail.com>
2022-12-15 20:42:03 -0300

    First commit

---
+++ main.js
@@ -0,0 +1,5 @@
+// Create a function that takes two numbers as arguments and return their sum.
+function add(a, b) {
+    return a + b;
+}
+console.log(add(1, 2));
---
+++ main.ts
@@ -0,0 +1,7 @@
+// Create a function that takes two numbers as arguments and return their sum.
+
+function add(a: number, b: number) {
+    return a + b;
+    }
+
+console.log(`O resultado é: ${add(1, 2)}`);
